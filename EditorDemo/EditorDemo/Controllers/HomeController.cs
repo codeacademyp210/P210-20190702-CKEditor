250f8151231f266f3d99b09dc5e0e6c5e0c1d838
Sattar Amrahov <settar216@gmail.com>
2019-07-02 18:33:14 +0400

    files added

---
+++ EditorDemo/EditorDemo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using EditorDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EditorDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EditorDemoEntities db = new EditorDemoEntities();
+
+        public ActionResult Index()
+        {
+            string cName = Thread.CurrentThread.CurrentUICulture.Name;
+
+            return View(db.Packages.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # P210-20190702-CKEditor
+
+Faydalı linklər:
+
+- https://ckeditor.com/ckeditor-4/demo/
+- https://cdn.ckeditor.com/
+- https://www.tiny.cloud/
